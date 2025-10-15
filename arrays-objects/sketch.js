0d0f5674f18e080fe540e4bb13597a82437aa2d5
KrishithaS <7003354@spsd.sk.ca>
2025-10-15 09:53:53 -0600

    new demo and updates on assignment array

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,11 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x = 220;
+let y = 320;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  showRects();
 }
 
 function draw() {
-  background(220);
+  // background(220);
+  // showRects();
+}
+
+function showRects(){
+  for (let i = 0; i < 7; i ++){
+    for (let o = 0; o < 3; o ++){
+      rect(x*i, y*o, 190, 290);
+    }
+  }
 }
