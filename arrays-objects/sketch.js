b81c4af9a0616eacd9e1cacdbcb77d231a17b70c
KrishithaS <7003354@spsd.sk.ca>
2025-10-16 09:52:43 -0600

    make the cards neater in the assignment

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -6,7 +6,8 @@
 // - describe what you did to take this project "above and beyond"
 
 let x = 220;
-let y = 320;
+let y = 300;
+let allRects = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,9 +20,20 @@ function draw() {
 }
 
 function showRects(){
-  for (let i = 0; i < 7; i ++){
+  fill(random(255), random(255), random(255));
+  for (let i = 0; i < 8; i ++){
     for (let o = 0; o < 3; o ++){
-      rect(x*i, y*o, 190, 290);
+      rect(100 + x*i, 10 + y*o, 190, 280);
     }
   }
+}
+
+function spawnObject(){
+  let rects = {
+    x: 220,
+    y: 320,
+    w: 190,
+    h: 290,
+  };
+  return rects;
 }
