70dd96a9587b75283d3c53e944ab43f53c146dd0
KrishithaS <7003354@spsd.sk.ca>
2025-10-26 18:09:02 -0600

    see if the cards match made improvement in mouse clicked

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -3,53 +3,158 @@
 // 10 October, 2025
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - shuffle
 
-let cWidth = 200;
-let cHeight = 280;
-let i = 0;
-let o = 0;
-let cards = [];
+//variables
+
+//card width
+let cWidth = 120;
+
+//card height
+let cHeight = 180;
+
+//starting position
+let x = 150; 
+let y = 100;
+
+let cards = []; //array
+let firstCard = -1;
+let secondCard = -1;
+
+let flipTime = 0;
+let standardTime = 600;
+
+let gapX = 40;
+let gapY = 30;
+
+let canClick = true;
 
 function setup() {
   createCanvas(900, 900);
-  showRects();
+
+  //create rectangles for cards
+  createRects();
 }
 
 function draw() {
-  // background(220);
-  // showRects();
+  background(200);
+
+  //display cards
+  showRects();
+
+  //check if cards match
+  isMatch();
 }
 
-function showRects(){
-  for(let rects of cards){
-    fill(rects.r, rects.b, rects.g);
-    // for (let i = 0; i < width; i ++){
-    //   for (let o = 0; o < height; o ++){
-    rect(rects.x, rects.y, rects.w, rects.h);
+function createRects(){
+
+  // card colors
+  let colors = ["red", "blue", "green", "yellow", "pink", "orange"];
+  let allColors = [];
+
+  for (let i = 0; i < colors.length; i++){
+    allColors.push(colors[i]);
+    allColors.push(colors[i]);
   }
-  //   }
-  // }
+
+  //Shuffle colors randomly
+  shuffle(allColors, true);
+
+  for(let i = 0; i < allColors.length; i ++){
+    spawnObject(allColors[i]);
+  }
+
+// set cards in 3 x 4 grid
+  let startX = 150;
+  let startY = 100;
+
+  for(let i = 0; i < cards.length; i++){
+    cards[i].x = x;
+    cards[i].y = y;
+    cards[i].revealed = false;
+    cards[i].matched = false;
+
+    x += cWidth + gapX;
+
+    if ((i + 1) % 4 === 0){
+      x = startX;
+      y += cHeight + gapY;
+    }
+  }
 }
 
-function spawnObject(){
+//create single rectangle/card
+function spawnObject(cardColor){
   let rects = {
-    x: 100,
-    y: 100,
+    x: x,
+    y: y,
     w: cWidth,
     h: cHeight,
-    r: random(225),
-    b: random(225),
-    g: random(255),
+    color: cardColor,
+    revealed: false,
+    matched: false
   };
+
+  //add card to array
   cards.push(rects);
 }
 
+//display cards on canvas
+function showRects(){
+  for (let rects of cards) {
+    if (rects.revealed || rects.matched) {
+      fill(rects.color);
+    }
+    else{
+      fill(220);
+    }
+    rect(rects.x, rects.y, rects.w, rects.h);
+  }
+}
 
+// check is mouse is over a card and if the card is clicked
 function mousePressed(){
-  for(let rects of cards){
-    if (mouseX > rects.x && mouseX < rects.x + rects.w && mouseY > rects.y && mouseY < rects.y + rects.h){
+  if (canClick){
+    for(let i = 0; i < cards.length; i++){
+      let rects = cards[i];
 
+      if (mouseX > rects.x && mouseX < rects.x + rects.w && mouseY > rects.y && mouseY < rects.y + rects.h){
+        
+        if(!rects.revealed && !rects.matched) {
+          rects.revealed = true;
+
+          if(firstCard === -1){
+            firstCard = i;
+          }
+          else if(secondCard === -1){
+            secondCard = i;
+            canClick = false;
+            flipTime = millis();
+          }
+        }
+      }
+    }
+  }
+}
+
+// check if the color on the first card match the color on the second card
+function isMatch(){
+  if(firstCard !== -1 && secondCard !== -1){
+
+    if(millis() - flipTime > standardTime){
+
+      if(cards[firstCard].color === cards[secondCard].color){
+        cards[firstCard].matched = true;
+        cards[secondCard].matched = true;
+      }
+      else {
+        cards[firstCard].revealed = false;
+        cards[secondCard].revealed = false;
+      }
+
+      firstCard = -1;
+      secondCard = -1;
+      canClick = true;
     }
   }
 }
