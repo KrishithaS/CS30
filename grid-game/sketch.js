388975140f34ed5a47afa8c8929b29ec8297b365
KrishithaS <7003354@spsd.sk.ca>
2025-10-31 09:44:42 -0600

    basic update to grid assignment

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -5,11 +5,28 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const CELL_SIZE = 50;
+let rows;
+let cols;
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cols = Math.floor(width/CELL_SIZE);
+  rows = Math.floor(height/CELL_SIZE);
+  grid = generateRandomGrid(cols, rows);
 }
 
 function draw() {
   background(220);
+}
+
+
+function generateRandomGrid(cols, rows){
+  let newGrid = [];
+  for(let y = 0; y < rows; y ++){
+    newGrid.push([]);
+    for(let x = 0; x < cols; x ++){
+    }
+  }
 }
