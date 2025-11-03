256d3e209e90c82efe18a86a552494a869367bb8
KrishithaS <7003354@spsd.sk.ca>
2025-11-03 09:54:06 -0600

    demo and update assignment

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 const CELL_SIZE = 50;
+const NUMBER_OF_SQUARES = 4;
 let rows;
 let cols;
 let grid;
@@ -19,14 +20,31 @@ function setup() {
 
 function draw() {
   background(220);
+  displayGrid();
 }
 
 
-function generateRandomGrid(cols, rows){
-  let newGrid = [];
-  for(let y = 0; y < rows; y ++){
-    newGrid.push([]);
-    for(let x = 0; x < cols; x ++){
+// function generateRandomGrid(cols, rows){
+//   let newGrid = [];
+//   for(let y = 0; y < rows; y ++){
+//     newGrid.push([]);
+//     for(let x = 0; x < cols; x ++){
+//       if(random(100) < 50){
+//         newGrid[y].push(0);
+//       }
+//       else{
+//         newGrid[y].push(1);
+//       }
+//     }
+//   }
+//   return newGrid;
+// }
+
+function displayGrid(){
+  for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
+    for(let x = 0; x < NUMBER_OF_SQUARES; x ++){
+      fill("black");
+      square(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE);
     }
   }
 }
