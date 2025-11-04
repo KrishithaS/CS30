f48b71118081cdd6e7ecefade03a53f4f7cd5fad
KrishithaS <7003354@spsd.sk.ca>
2025-11-04 09:54:21 -0600

    Created empty grid 4 X 4

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -5,17 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const CELL_SIZE = 50;
+let cellSize;
 const NUMBER_OF_SQUARES = 4;
 let rows;
 let cols;
 let grid;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cols = Math.floor(width/CELL_SIZE);
-  rows = Math.floor(height/CELL_SIZE);
-  grid = generateRandomGrid(cols, rows);
+  createCanvas(windowWidth * 0.443, windowHeight *0.9);
+  if(width < height){
+    cellSize = Math.floor(width/NUMBER_OF_SQUARES);
+  }
+  else{
+  }
+  cellSize = Math.floor(height/NUMBER_OF_SQUARES);
+  grid = generateEmptyGrid(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
 }
 
 function draw() {
@@ -23,28 +27,47 @@ function draw() {
   displayGrid();
 }
 
+function gameStart(){
+}
 
-// function generateRandomGrid(cols, rows){
-//   let newGrid = [];
-//   for(let y = 0; y < rows; y ++){
-//     newGrid.push([]);
-//     for(let x = 0; x < cols; x ++){
-//       if(random(100) < 50){
-//         newGrid[y].push(0);
-//       }
-//       else{
-//         newGrid[y].push(1);
-//       }
-//     }
-//   }
-//   return newGrid;
-// }
+
+
+function keyPressed(){
+  if(key === "w"){
+    //move up
+  }
+  else if(key === "s"){
+    //move down
+  }
+  else if(key === "a"){
+    //move left
+  }
+  else if(key === "d"){
+    //move right
+  }
+  else if(key === "r"){
+    //reset / start a new game
+  }
+}
+
+
+function generateEmptyGrid(cols, rows){
+  let newGrid = [];
+  for(let y = 0; y < rows; y ++){
+    newGrid.push([]);
+    for(let x = 0; x < cols; x ++){
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+}
 
 function displayGrid(){
   for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
     for(let x = 0; x < NUMBER_OF_SQUARES; x ++){
-      fill("black");
-      square(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE);
+      if(grid[y][x] === 0){
+        square(x*cellSize, y*cellSize, cellSize);
+      }
     }
   }
 }
