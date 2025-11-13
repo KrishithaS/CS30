66b98b74fe89594c63c4722312b35196435fd3d6
KrishithaS <7003354@spsd.sk.ca>
2025-11-12 22:18:37 -0600

    finished move able blocks

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -13,8 +13,6 @@ let grid;
 let gameOn = false;
 let score = 0;
 let move = false;
-// let startPoint = height/2;
-// let gameStart = false;
 
 function setup() {
   createCanvas(windowWidth * 0.3, windowHeight);
@@ -25,44 +23,42 @@ function setup() {
     cellSize = Math.floor(height/NUMBER_OF_SQUARES);
   }
   grid = generateEmptyGrid(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
+
+  generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
+  generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
 }
 
 function draw() {
   background(220);
   displayGrid();
   displayScore();
-  if(gameOn){
-    updateGrid();
-  }
-  // start();
 }
-
-// function start(){
-//   if (gameStart === turn){
-//     generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
-//   }
-// }
 
 
 
 function keyPressed(){
+  move = false;
+
   if(key === "w"){
     moveUp();
-    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
   }
   else if(key === "s"){
     moveDown();
-    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
   }
   else if(key === "a"){
     moveLeft();
-    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
   }
   else if(key === "d"){
     moveRight();
-    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
   }
   else if(key === "r"){
+    grid = generateEmptyGrid(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
+    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
+    generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
+    score = 0;
+  }
+
+  if (move === true){
     generateTwoOrFourSquare(NUMBER_OF_SQUARES, NUMBER_OF_SQUARES);
   }
 }
@@ -91,7 +87,7 @@ function generateTwoOrFourSquare(cols, rows){
     }
   }
   if(twoOrFourX.length > 0){
-    let randomPick = random(twoOrFourX.length);
+    let randomPick = Math.floor(random(twoOrFourX.length));
     let x = twoOrFourX[randomPick];
     let y = twoOrFourY[randomPick];
     if(random(100) > 50){
@@ -103,87 +99,107 @@ function generateTwoOrFourSquare(cols, rows){
   }
 }
 
-function moveRight(grid){
-  for(let x = 0; x < NUMBER_OF_SQUARES; x++){
-    while(x < NUMBER_OF_SQUARES && grid[y][x] === 0){
-      x = x - 1;
+function moveRight(){
+  move = false;
+
+  for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
+    for(let x = NUMBER_OF_SQUARES - 2; x >= 0; x --){
+
+      if(grid[y][x] !== 0){
+        let nowPos = x;
+
+        while(nowPos < NUMBER_OF_SQUARES - 1 && grid[y][nowPos + 1] === 0){
+          grid[y][nowPos + 1] = grid[y][nowPos];
+          grid[y][nowPos] = 0;
+          nowPos = nowPos + 1;
+          move = true;
+        }
+
+        if(nowPos < NUMBER_OF_SQUARES - 1 && grid[y][nowPos + 1] === grid[y][nowPos]){
+          grid[y][nowPos + 1] = grid[y][nowPos + 1] + grid[y][nowPos];
+          grid[y][nowPos] = 0;
+          score = score + grid[y][nowPos + 1];
+          move = true;
+        }
+      }
     }
   }
 }
 
-function moveLeft(grid){
-  let nowX; // current place of the block
+function moveLeft(){
+  move = false;
   for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
     for(let x = 1; x < NUMBER_OF_SQUARES; x ++){
+
       if(grid[y][x] !== 0){
-        nowX = x;
-      }
-      while(nowX > 0 && grid[y][nowX - 1] === 0){
-        grid[y][nowX - 1] = grid[y][nowX];
-        grid[y][nowX] = 0;
-        nowX = nowX - 1;
-      }
+        let nowPos = x;
 
-      if(nowX > 0 && grid[y][nowX - 1] === grid[y][nowX]){
-        grid[y][nowX - 1] = grid[y][nowX - 1] + grid[y][now];
+        while(nowPos > 0 && grid[y][nowPos - 1] === 0){
+          grid[y][nowPos - 1] = grid[y][nowPos];
+          grid[y][nowPos] = 0;
+          nowPos = nowPos - 1;
+          move = true;
+        }
+
+        if(nowPos > 0 && grid[y][nowPos - 1] === grid[y][nowPos]){
+          grid[y][nowPos - 1] = grid[y][nowPos - 1] + grid[y][nowPos];
+          grid[y][nowPos] = 0;
+          score = score + grid[y][nowPos - 1];
+          move = true;
+        }
       }
     }
   }
 }
 
-function moveUp(grid){
-  for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
-    while(y > 0 && grid[y][x] === 0){
-      y = y - 1;
+function moveUp(){
+  move = false;
+  for(let x = 0; x < NUMBER_OF_SQUARES; x ++){
+    for(let y = 1; y < NUMBER_OF_SQUARES; y ++){
+
+      if(grid[y][x] !== 0){
+        let nowPos = y;
+
+        while(nowPos > 0 && grid[nowPos - 1][x] === 0){
+          grid[nowPos - 1][x] = grid[nowPos][x];
+          grid[nowPos][x] = 0;
+          nowPos = nowPos - 1;
+          move = true;
+        }
+
+        if(nowPos > 0 && grid[nowPos - 1][x] === grid[nowPos][x]){
+          grid[nowPos - 1][x] = grid[nowPos - 1][x] + grid[nowPos][x];
+          grid[nowPos][x] = 0;
+          score = score +  grid[nowPos - 1][x];
+          move = true;
+        }
+      }
     }
   }
 }
 
-function moveDown(grid){
-  for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
-    while(y < NUMBER_OF_SQUARES && grid[y][x] === 0){
-      y = y + 1;
-    }
-  }
-}
+function moveDown(){
+  move = false;
+  for(let x = 0; x < NUMBER_OF_SQUARES; x ++){
+    for(let y = NUMBER_OF_SQUARES - 2; y >= 0; y --){
 
-function updateGrid(){
-  for(let y = 0; y < NUMBER_OF_SQUARES; y++){
-    for(let x = 0; x < NUMBER_OF_SQUARES; x++){
-      if(grid[y][x] === 2){
-        fill("#e7dbcfff");
+      if(grid[y][x] !== 0){
+        let nowPos = y;
+
+        while(nowPos < NUMBER_OF_SQUARES - 1 && grid[nowPos + 1][x] === 0){
+          grid[nowPos + 1][x] = grid[nowPos][x];
+          grid[nowPos][x] = 0;
+          nowPos = nowPos + 1;
+          move = true;
+        }
+
+        if(nowPos < NUMBER_OF_SQUARES - 1 && grid[nowPos + 1][x] === grid[nowPos][x]){
+          grid[nowPos + 1][x] = grid[nowPos + 1][x] + grid[nowPos][x];
+          grid[nowPos][x] = 0;
+          score = score +  grid[nowPos + 1][x];
+          move = true;
+        }
       }
-      else if(grid[y][x] === 4){
-        fill("#e2d4baff");
-      }
-      else if(grid[y][x] === 8){
-        fill("#F2B179");
-      }
-      else if(grid[y][x] === 16){
-        fill("#F59563");
-      }
-      else if(grid[y][x] === 32){
-        fill("#F67C5F");
-      }
-      else if(grid[y][x] === 64){
-        fill("#F65E3B");
-      }
-      else if(grid[y][x] === 128){
-        fill("#EDCF72");
-      }
-      else if(grid[y][x] === 256){
-        fill("#EDCC61");
-      }
-      else if(grid[y][x] === 512){
-        fill("#EDC850");
-      }
-      else if(grid[y][x] === 1024){
-        fill("#EDC53F");
-      }
-      else if(grid[y][x] === 2048){
-        fill("#EDC22E");
-      }
-      square(x*cellSize, height/3 + y*cellSize, cellSize);
     }
   }
 }
@@ -191,11 +207,56 @@ function updateGrid(){
 function displayGrid(){
   for(let y = 0; y < NUMBER_OF_SQUARES; y ++){
     for(let x = 0; x < NUMBER_OF_SQUARES; x ++){
-      if(grid[y][x] === 0){
-        fill("#c9bfb5ff");
+      fill("#c9bfb5ff");
+      stroke("#928271ff");
+      strokeWeight(5);
+      square(x*cellSize, height/3 + y*cellSize, cellSize);
+
+      if(grid[y][x] !== 0){
+
+        if(grid[y][x] === 2){
+          fill("#e7dbcfff");
+        }
+        else if(grid[y][x] === 4){
+          fill("#e2d4baff");
+        }
+        else if(grid[y][x] === 8){
+          fill("#F2B179");
+        }
+        else if(grid[y][x] === 16){
+          fill("#F59563");
+        }
+        else if(grid[y][x] === 32){
+          fill("#F67C5F");
+        }
+        else if(grid[y][x] === 64){
+          fill("#F65E3B");
+        }
+        else if(grid[y][x] === 128){
+          fill("#EDCF72");
+        }
+        else if(grid[y][x] === 256){
+          fill("#EDCC61");
+        }
+        else if(grid[y][x] === 512){
+          fill("#EDC850");
+        }
+        else if(grid[y][x] === 1024){
+          fill("#EDC53F");
+        }
+        else if(grid[y][x] === 2048){
+          fill("#EDC22E");
+        }
+        noStroke();
         stroke("#928271ff");
-        strokeWeight(5);
         square(x*cellSize, height/3 + y*cellSize, cellSize);
+
+        if(grid[y][x] !== 0){
+          textAlign(CENTER, CENTER);
+          textSize(cellSize * 0.5);
+          fill(0);
+          text(grid[y][x], x*cellSize + cellSize/2, height/3 + y*cellSize + cellSize/2);
+        }
       }
     }
   }
@@ -204,5 +265,6 @@ function displayGrid(){
 
 function displayScore(){
   textAlign(CENTER);
-  text("Score: ");
+  textSize(40);
+  text("Score: " + score, width/2, 100);
 }
